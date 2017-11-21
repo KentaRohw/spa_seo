acbabe7079e926ab7ce411732fefdfe93832ad30
Kentaro TANAKA <kentatan@yahoo-corp.jp>
2017-11-21 18:59:05 +0900

    【update】CardList Width

--- src/component/CardList.js
+++ src/component/CardList.js
@@ -16,11 +16,11 @@ const CardList = () => (
   <div>
     <Flex wrap>
       {ENTRYS.map(entry => (
-        <Box key={entry.id} px={2} py={2} w={[1, 1/2, 210]}>
+        <Box key={entry.id} px={2} py={2} w={[1, 1/2, 250]}>
           <Card>
             <BlockLink href={`/entry/${entry.author}/${entry.id}`}>
               <BackgroundImage ratio={1} mb={[1]} src={entry.image_url}/>
-              <Subhead p={2} mb={2} f={[2]} color='#333'>{entry.title}</Subhead>
+              <Subhead p={2} mb={2} f={[1]} color='#333'>{entry.title}</Subhead>
               <Box p={2}>
                 <Relative>
                   <Absolute bottom left><Small color='#999'>{entry.date}</Small></Absolute>
