705aec702ea548e6c2971abf184c9379d94ab1e7
Kentaro TANAKA <kentatan@yahoo-corp.jp>
2017-11-29 17:59:59 +0900

    【add】react-helmet
    https://github.com/nfl/react-helmet

--- src/component/About.js
+++ src/component/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import { 
   Flex,
   Box,
@@ -8,6 +9,9 @@ import {
   
 const About = () => (
   <div>
+    <Helmet
+      title="About"
+    />
     <Flex wrap mx='auto' mb={[5]} w={[1, null, null, 1024]}>
       <Box w={[1, null, null, 1024]} px={2} mb={4}>
         <Border mb={2} pb={2} bottom>
