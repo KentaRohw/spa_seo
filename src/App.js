7d9418a12a7140ab5305dab76c0bf11ba900d191
Kentaro TANAKA <kentatan@yahoo-corp.jp>
2018-02-02 17:55:44 +0900

    [add]babel-polyfill

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 import { Provider } from 'rebass';
--- src/component/Entrys.js
+++ src/component/Entrys.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Helmet from 'react-helmet';
